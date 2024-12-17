24be015b043b0c723e2715c78f95a23d284ac668
malxcdev <malxcondev@gmail.com>
2024-12-17 10:37:28 -0500

    pequeños cambios caja

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -12,59 +12,73 @@ function Calculator() {
             Clear All
           </a>
         </div>
-        <form>
-          <div className="flex flex-col gap-2 ">
+        <form className="">
+          <div className="flex flex-col gap-2 mb-6">
             <label className="text-slate-700 font-medium">
               Mortgage Amount
             </label>
             <div className="relative flex items-center">
-              <span className="absolute left-0 rounded-l-md font-bold text-gray-500 text-lg pointer-events-none bg-slate-200 h-full px-4 justify-center items-center flex">
+              <span className="absolute left-0 rounded-l-md font-bold text-gray-500 text-lg pointer-events-none bg-slate-200 px-4 justify-center items-center flex z-0 h-full">
                 £
               </span>
               <input
                 id="mortgage"
                 type="text"
                 value="300,000"
-                className="pl-14 pr-4 py-2 border-3 border-gray-300 rounded-md overflow-hidden focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 text-gray-900 font-semibold text-lg w-full"
+                className="pl-14 pr-4 py-2 border-2 border-gray-400 rounded-md overflow-hidden focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 text-gray-900 font-semibold text-lg w-full z-10"
               />
             </div>
           </div>
-          <div>
-            <div>
+          <div className="">
+            <div className="mb-6">
               <label className="text-slate-700 font-medium">Mortgage Term</label>
               <div className="relative flex items-center">
-                <span className="absolute right-0 rounded-l-md font-bold text-gray-500 text-lg pointer-events-none bg-slate-200 h-full px-4 justify-center items-center flex">
+                <span className="absolute right-0 rounded-l-md font-bold text-gray-500 text-lg pointer-events-none bg-slate-200 h-full px-4 justify-center items-center flex z-0">
                   years
                 </span>
                 <input
                   id="mortgage"
                   type="text"
                   value="25"
-                  className="pl-4 pr-4 py-2 border-3 border-gray-300 rounded-md overflow-hidden focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 text-gray-900 font-semibold text-lg w-full"
+                  className="pl-4 pr-4 py-2 border-2 border-gray-400 rounded-md overflow-hidden focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 text-gray-900 font-semibold text-lg w-full z-10"
                 />
               </div>
             </div>
-            <div>
+            <div className="mb-6">
               <label className="text-slate-700 font-medium">Interest Rate</label>
               <div className="relative flex items-center">
-                <span className="absolute right-0 rounded-l-md font-bold text-gray-500 text-lg pointer-events-none bg-slate-200 h-full px-4 justify-center items-center flex">
+                <span className="absolute right-0 rounded-l-md font-bold text-gray-500 text-lg pointer-events-none bg-slate-200 h-full px-4 justify-center items-center flex z-0">
                   %
                 </span>
                 <input
                   id="mortgage"
                   type="text"
                   value="25"
-                  className="pl-4 pr-4 py-2 border-3 border-gray-300 rounded-md overflow-hidden focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 text-gray-900 font-semibold text-lg w-full"
+                  className="pl-4 pr-4 py-2 border-2 border-gray-400 rounded-md overflow-hidden focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 text-gray-900 font-semibold text-lg w-full z-10"
                 />
               </div>
             </div>
           </div>
-          <div>
-            <label>Mortgage Type</label>
-            <label>
-              <input type="checkbox" name="" id="" />
+          <div className="flex flex-col gap-2">
+            <label className="text-slate-700 font-medium">Mortgage Type</label>
+            <div className="flex items-center gap-2 px-4 py-3 border-2 border-slate-400 rounded-md font-bold cursor-pointer text-slate-900" id="repayment-container">
+              <input
+                type="checkbox"
+                id="repayment-checkbox"
+                name="repayment-checkbox"
+                className="w-5 h-5 border-2 border-gray-500 rounded-full appearance-none cursor-pointer "
+                onChange={(e) => {
+                  const container = document.getElementById('repayment-container');
+                  if (e.target.checked) {
+                    container.classList.add('bg-lime', 'border-blue-500');
+                  } else {
+                    container.classList.remove('bg-lime', 'border-blue-500');
+                  }
+                }}
+              />
               Repayment
-            </label>
+            </div>
+
             <label>
               <input type="checkbox" name="" id="" />
               Interest Only
